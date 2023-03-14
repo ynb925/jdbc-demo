d46b85b9b78211a18734716709c1bdac03240c71
ynb925 <ynb4mail@gmail.com>
2023-03-14 23:04:34 +0200

    init demo conn jdbc and postgresql, all cfg is default and made for test to connect with database

---
+++ src/JdbcDemoApp.java
@@ -0,0 +1,32 @@
+import lombok.SneakyThrows;
+import org.postgresql.ds.PGSimpleDataSource;
+
+import java.sql.SQLException;
+
+public class JdbcDemoApp {
+
+    @SneakyThrows
+    public static void main(String[] args) {
+        PGSimpleDataSource dataSource = new PGSimpleDataSource();
+        dataSource.setURL("jdbc:postgresql://localhost:5432/postgres");
+        dataSource.setUser("postgres");
+        dataSource.setPassword("admin5555");
+
+        try (var connection = dataSource.getConnection()) {
+            try (var statement = connection.createStatement()) {
+                var resultSet = statement.executeQuery("select * from doctor;");
+                while (resultSet.next()) {
+                    var fullName = resultSet.getString("fullname");
+                    var spez = resultSet.getString("spez");
+                    var age = resultSet.getString("age");
+
+                    System.out.println(fullName + " - " + spez + " - " + age);
+                }
+            } catch (SQLException e) {
+                throw new RuntimeException(e);
+            }
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
